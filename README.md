c28482b49e90431327515614164d4d28389c8bc2
José Castro <a.j.castro.b@gmail.com>
2026-04-17 02:19:41 -0500

    teminado primer ejercicio-colorear página

---
+++ README.md
@@ -0,0 +1,9 @@
+# Entrega ejercicios JavaScript
+
+Página diseñada para la entrega de los ejercicios del módulo de JavaScript para la academia Conquerblocks.
+
+![HTML 5]()
+
+![SASS](https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![Vite](https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white)
--- pages/ejer1.js
+++ pages/ejer1.js
@@ -1,4 +1,32 @@
 import "../sass/main.scss";
 import { showMenu } from "../src/utils/nav";
 
+//Generar un color random.
+const createRandomColor = () => {
+  //Escoger el color aleatorio.
+  let rojo = Math.floor(Math.random() * 255);
+  let verde = Math.floor(Math.random() * 255);
+  let azul = Math.floor(Math.random() * 255);
+
+  return `rgb(${rojo},${verde},${azul})`;
+};
+
+//Cambiar el color del body con un color random y actualizar la etiqueta que marca el color.
+const createTagColor = (btnId) => {
+  //Seleccionar las etiquetas y el botón.
+  const btn = document.getElementById(btnId);
+  const body = document.querySelector("body");
+  let badge = document.querySelector(".color");
+
+  btn.addEventListener("click", (e) => {
+    //Evitar la propagación hacia el div padre.
+    e.stopPropagation();
+    let color = createRandomColor();
+    body.style.backgroundColor = `${color}`;
+    badge.textContent = color;
+    return color;
+  });
+};
+
 showMenu("nav-toggle", "nav-menu");
+createTagColor("btn-color");
