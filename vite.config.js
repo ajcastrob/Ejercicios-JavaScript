b17ac8d2397516ab37e2070ac83dcf292218ba90
José Castro <a.j.castro.b@gmail.com>
2026-04-20 21:01:31 -0500

    Fix: rutas relativas para Github pages

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "./",
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
         ejer1: resolve(__dirname, "pages/ejer1.html"),
         ejer2: resolve(__dirname, "pages/ejer2.html"),
+        ejer3: resolve(__dirname, "pages/ejer3.html"),
+        ejer4: resolve(__dirname, "pages/ejer4.html"),
+        ejer5: resolve(__dirname, "pages/ejer5.html"),
+        ejer6: resolve(__dirname, "pages/ejer6.html"),
+        ejer7: resolve(__dirname, "pages/ejer7.html"),
+        ejer8: resolve(__dirname, "pages/ejer8.html"),
+        ejer9: resolve(__dirname, "pages/ejer9.html"),
       },
     },
   },
